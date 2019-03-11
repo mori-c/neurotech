67493359b61ce06464ad45488d7c6aa8a6c9f1c1
mori-c <moriconstant@gmail.com>
2019-03-11 17:38:19 -0400

    update heroes.component.ts

--- 06-angular/sandbox/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ 06-angular/sandbox/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+// https://angular.io/tutorial/toh-pt1
+// 
+// $ ng generate component heroes
+// .. src/app/heroes/
+// .. HeroesComponent/{4 files}
 
-@Component({
+
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+// About @Component
+// decorator function
+// hosts ng metadata: 
+// app-heroes/../selector (css) == heroes.components.html
+// templateUrl >> this file is a template & where it can be found 
+// styleUrls >> private css location
+
+@Component( {
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
-export class HeroesComponent implements OnInit {
+  styleUrls: [ './heroes.component.css' ]
+} )
+export class HeroesComponent implements OnInit
+{
 
-  constructor() { }
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
-  ngOnInit() {
-  }
+  // title = 'Windstorm';
+
+  constructor () { }
+
+  ngOnInit() { }
 
 }
